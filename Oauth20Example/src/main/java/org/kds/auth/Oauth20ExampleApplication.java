ba70b9cddf239da67d1964f9d111cefa6268e0f8
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2020-01-11 12:17:13 +1100

    Adding auth server

--- Oauth20Example/src/main/java/org/kds/auth/Oauth20ExampleApplication.java
+++ Oauth20Example/src/main/java/org/kds/auth/Oauth20ExampleApplication.java
@@ -2,8 +2,10 @@ package org.kds.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class Oauth20ExampleApplication {
 
 	public static void main(String[] args) {
