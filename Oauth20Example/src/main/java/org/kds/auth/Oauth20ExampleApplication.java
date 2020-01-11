8e533fd8e1c30f069cadcb51dc8e0d7ed48bda7d
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2020-01-11 11:22:58 +1100

    Adding initial structure

---
+++ Oauth20Example/src/main/java/org/kds/auth/Oauth20ExampleApplication.java
@@ -0,0 +1,13 @@
+package org.kds.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth20ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth20ExampleApplication.class, args);
+	}
+
+}
---
+++ Oauth20Example/src/test/java/org/kds/auth/Oauth20ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kds.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth20ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
